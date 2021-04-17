279e5af2d1d1fe057e011070bf94dcef8f4081ac
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-17 17:56:14 -0600

    add seeder

---
+++ backend/seeder.js
@@ -0,0 +1,21 @@
+import connectDb from "./config/db.js";
+import Vehicle from "./models/Vehicle.js";
+import carData from "./data/carData.js";
+
+connectDb();
+
+const importData = async () => {
+  try {
+    await Vehicle.deleteMany();
+
+    await Vehicle.insertMany(carData);
+
+    console.log("Data Imported");
+    process.exit(1);
+  } catch (error) {
+    console.error(`${error}`);
+    process.exit(1);
+  }
+};
+
+importData();
