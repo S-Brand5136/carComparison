27bc6ae0db197b8609883363e04014b2832fdadd
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-17 17:51:14 -0600

    add data set to seed db

---
+++ backend/data/carData.js
@@ -0,0 +1,66 @@
+const car = [
+  {
+    manufacturer: "Toyota",
+    model: "Corolla",
+    price: 19600,
+    engineSize: "1.8 liter 139hp",
+    numSeats: 5,
+    milesPerGallon: 33,
+    safetyRating: 5,
+    specialFeatures: [
+      {
+        heatedSeats: true,
+        sunRoof: false,
+        remoteStart: true,
+      },
+    ],
+  },
+  {
+    manufacturer: "Toyota",
+    model: "Avalon",
+    price: 36125,
+    engineSize: "2.5 liter 205hp",
+    numSeats: 5,
+    milesPerGallon: 28,
+    safetyRating: 5,
+    specialFeatures: [
+      {
+        heatedSeats: true,
+        sunRoof: true,
+        remoteStart: true,
+      },
+    ],
+  },
+  {
+    manufacturer: "Audi",
+    model: "A4",
+    price: 39100,
+    engineSize: "2.0 liter 261hp",
+    numSeats: 5,
+    milesPerGallon: 27,
+    safetyRating: 5,
+    specialFeatures: [
+      {
+        heatedSeats: true,
+        sunRoof: false,
+        remoteStart: true,
+      },
+    ],
+  },
+  {
+    manufacturer: "Honda",
+    model: "Accord",
+    price: 24970,
+    engineSize: "1.5 liter 192hp",
+    numSeats: 5,
+    milesPerGallon: 30,
+    safetyRating: 5,
+    specialFeatures: [
+      {
+        heatedSeats: false,
+        sunRoof: false,
+        remoteStart: true,
+      },
+    ],
+  },
+];
