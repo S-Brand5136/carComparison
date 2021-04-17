16883892953bd632931c2dfb0678a651c15f2766
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-16 18:41:05 -0600

    add update and delete controller

--- backend/controllers/carController.js
+++ backend/controllers/carController.js
@@ -13,7 +13,7 @@ const getVehicles = asyncHandler(async (req, res) => {
 // @route   GET /api/Vehicles/:model
 // @access  Public
 const getVehicleByModel = asyncHandler(async (req, res) => {
-  const vehicle = Vehicle.find(req.params.model);
+  const vehicle = await Vehicle.find(req.params.model);
 
   if (vehicle) {
     res.json({ vehicle });
@@ -27,7 +27,7 @@ const getVehicleByModel = asyncHandler(async (req, res) => {
 // @route   GET /api/Vehicles/:manufacturer
 // @access  Public
 const getVehiclesByManufacturer = asyncHandler(async (req, res) => {
-  const vehicles = Vehicle.find(req.params.manufacturer);
+  const vehicles = await Vehicle.find(req.params.manufacturer);
 
   if (vehicles) {
     res.json(vehicles);
@@ -40,11 +40,49 @@ const getVehiclesByManufacturer = asyncHandler(async (req, res) => {
 // @desc    Edit Vehicle
 // @route   GET /api/Vehicles/:id
 // @access  Private
+const updateVehicle = asyncHandler(async (req, res) => {
+  const vehicle = await Vehicle.find(req.params.id);
+
+  if (vehicle) {
+    const {
+      manufacturer,
+      model,
+      price,
+      engineSize,
+      numSeats,
+      milesPerGallon,
+      safetyRating,
+      specialFeatures,
+    } = req.body;
+
+    vehicle.manufacturer = manufacturer;
+    vehicle.model = model;
+    vehicle.price = price;
+    vehicle.engineSize = engineSize;
+    vehicle.numSetas = numSeats;
+    vehicle.milesPerGallon = milesPerGallon;
+    vehicle.safetyRating = safetyRating;
+    vehicle.specialFeatures = specialFeatures;
+
+    const updatedVehicle = await vehicle.save();
+    res.status(201).json(updatedVehicle);
+  } else {
+    res.status(404);
+    throw new Error("Vehicle not found");
+  }
+});
 
 // @desc    Delete Vehicle
 // @route   GET /api/Vehicles/:id
 // @access  Private
+const deleteVehicle = asyncHandler(async (req, res) => {
+  const vehicle = Vehicle.find(req.params.id);
 
-// @desc    Fetch All Vehicles
-// @route   GET /api/Vehicles
-// @access  Public
+  if (vehicles) {
+    await vehicle.remove();
+    res.json({ message: "The vehicle has been removed from database" });
+  } else {
+    res.status(404);
+    throw new Error("Vehicle not found");
+  }
+});
