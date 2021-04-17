74452c20a27419f29e9ec0e4937c6ff9a972ace9
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-04-17 17:33:52 -0600

    add server routes and connectDb function

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import helmet from "helmet";
+import connectDb from "./config/db.js";
+import CarRoutes from "./routes/CarRoutes";
 
 dotenv.config();
+
+connectDb();
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
+
+app.use("/api/vehicles/", CarRoutes);
 
 const PORT = process.env.PORT || 5000;
 
